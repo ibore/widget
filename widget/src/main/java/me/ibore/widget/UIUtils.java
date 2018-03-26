67546a74bf2e9dbdd7fcc60588c6c1596ee99c17
bore521@163.com <xie9210>
2018-03-26 17:41:43 +0800

    Initial commit

---
+++ widget/src/main/java/me/ibore/widget/UIUtils.java
@@ -0,0 +1,56 @@
+package me.ibore.widget;
+
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2018/3/26.
+ */
+
+public class UIUtils {
+
+
+    /**
+     * dp 转 px
+     *
+     * @param dpValue dp 值
+     * @return px 值
+     */
+    public static int dp2px(Context context, final float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px 转 dp
+     *
+     * @param pxValue px 值
+     * @return dp 值
+     */
+    public static int px2dp(Context context, final float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp 转 px
+     *
+     * @param spValue sp 值
+     * @return px 值
+     */
+    public static int sp2px(Context context, final float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px 转 sp
+     *
+     * @param pxValue px 值
+     * @return sp 值
+     */
+    public static int px2sp(Context context, final float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+}
