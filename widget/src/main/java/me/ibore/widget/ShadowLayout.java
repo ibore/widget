9a801db3329c5fe3e7707506ffc7ff3fa9209535
bore521@163.com <xie9210>
2018-09-30 17:55:56 +0800

    edit ShadowLayout 2018/0//30 1.1.9

--- widget/src/main/java/me/ibore/widget/ShadowLayout.java
+++ widget/src/main/java/me/ibore/widget/ShadowLayout.java
@@ -22,9 +22,9 @@ public class ShadowLayout extends LinearLayout {
         TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.ShadowLayout);
         int shape = ta.getInt(R.styleable.ShadowLayout_slShape, ShadowDrawable.SHAPE_ROUND);
         int bgColor = ta.getColor(R.styleable.ShadowLayout_slBgColor, Color.parseColor("#00000000"));
-        int shapeRadius = (int) ta.getDimension(R.styleable.ShadowLayout_slShadowRadius, 0F);
+        int shapeRadius = (int) ta.getDimension(R.styleable.ShadowLayout_slShapeRadius, 0F);
         int shadowColor = ta.getColor(R.styleable.ShadowLayout_slShadowColor, Color.parseColor("#00000000"));
-        int shadowRadius = (int) ta.getDimension(R.styleable.ShadowLayout_slShapeRadius, 0F);
+        int shadowRadius = (int) ta.getDimension(R.styleable.ShadowLayout_slShadowRadius, 0F);
         int offsetX = (int) ta.getDimension(R.styleable.ShadowLayout_slOffsetX, 0F);
         int offsetY = (int) ta.getDimension(R.styleable.ShadowLayout_slOffsetY, 0F);
         ta.recycle();
@@ -34,12 +34,12 @@ public class ShadowLayout extends LinearLayout {
     public void setShadowDrawable(int shape, int bgColor, int shapeRadius, int shadowColor, int shadowRadius, int offsetX, int offsetY) {
         ShadowDrawable.setShadowDrawable(this, new ShadowDrawable.Builder()
                 .setShape(shape)
+                .setBgColor(bgColor)
                 .setShapeRadius(shapeRadius)
                 .setShadowColor(shadowColor)
                 .setShadowRadius(shadowRadius)
                 .setOffsetX(offsetX)
                 .setOffsetY(offsetY)
-                .setBgColor(bgColor)
                 .builder());
         setPadding(shadowRadius + offsetX, shadowRadius - offsetY, shadowRadius - offsetX, shadowRadius + offsetY);
     }
